0fd1c5072272cf2c7aff18acdf1e4587cbcfb6c3
eugenol <eug@live.co.za>
2016-04-14 00:44:01 +0200

    wxpand main

--- main.c
+++ main.c
@@ -3,6 +3,13 @@ Operating Systems assignment
 
 */
 
+struct _process {
+	char ID[11];
+	int arrival;
+	int burst;
+	int priority;
+};
+
 int main(int argv, char **argv)
 {
 	// Parse Arguments
@@ -10,6 +17,19 @@ int main(int argv, char **argv)
 	// Store in a list all the inputs
 	// Run simulation, depending on choice
 	// Display results
+	FILE *fptr;
+	int scheduler;
+	int quantum;	
+	
+		
+	if (argv < 3)
+	{
+		printf("Usage is: sheduler <input file> <Scheduler> <quantum (optional)>")
+		return EXIT_FAILURE;
+	}
+	
+		
+	
 	
 	return 0;	
 }
