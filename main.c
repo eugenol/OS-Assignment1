5d324e343e7b6f496a0affbeee545f35a391ce94
eugenol <eug@live.co.za>
2016-04-14 00:34:16 +0200

    add main

--- main.c
+++ main.c
@@ -5,5 +5,11 @@ Operating Systems assignment
 
 int main(int argv, char **argv)
 {
+	// Parse Arguments
+	// Read in input file
+	// Store in a list all the inputs
+	// Run simulation, depending on choice
+	// Display results
 	
+	return 0;	
 }
