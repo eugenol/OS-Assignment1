0b46903e74881f21c69221fcea4efbb1c49fe7dc
eugenol <eug@live.co.za>
2016-04-15 13:36:06 +0200

    remove unneeded stuff

--- SJF.c
+++ SJF.c
@@ -1,8 +1,6 @@
 #include "SJF.h"
 #include "queue.h"
 
-#include "queue.h"
-
 int SJF(struct _process **process_queue, struct _process **ready_queue, struct _process **done_queue, int quantum)
 {
     return 0;
