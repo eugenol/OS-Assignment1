f18d083f84d11b8c532277009dc3fd99e00c7cf4
eugenol <eug@live.co.za>
2016-04-18 00:42:34 +0200

    clean up RR.c

--- RR.c
+++ RR.c
@@ -1,29 +1,29 @@
 #include "RR.h"
 #include "queue.h"
 #include <limits.h>
-#include <stdio.h>
 
 int RR(struct _process **process_queue, struct _process **ready_queue, struct _process **done_queue, int quantum)
 {
     int runtime = 0;
     int first_proc_time = 0;
     int next_run_time = 0;
-    int time_left;
+    int time_left = 0;
     int isempty = 0;
-    struct _process temp_proc;
+    struct _process temp_proc; = {{0},0,0,0,0,NULL,NULL,NULL};
     struct _process *iter;
     struct _process *temp_ptr;
     struct _process *temp_queue = NULL;
-    struct _data temp_time_data;
+    struct _data temp_time_data = {0, 0, NULL};
     
     // if there is a process queue, send first process to the ready queue, set runtime
     // to start time, otherwise exit
-    printf("Begin Scheduler\n");
+    // this gets the ball rolling
     if(*process_queue)
     {
         temp_proc = remove_proc_node_from_front(&*process_queue);
         add_proc_node(&*ready_queue,temp_proc);
         runtime = first_proc_time = temp_proc.arrival;
+        
         if((*process_queue)->next)
             next_run_time = (*process_queue)->next->arrival;
         else
@@ -33,18 +33,18 @@ int RR(struct _process **process_queue, struct _process **ready_queue, struct _p
     {
         return runtime;
     }    
-    printf("Run time: %d\n",runtime);
-    // processws waiting?
+    
+    // processws waiting in any of the queues?
+    // loop to consume process
     while( *process_queue || *ready_queue || temp_queue)
     {
+        // add waiting processes to the ready queue
         while(*process_queue)
         {
-
             if(runtime >= (*process_queue)->arrival)
             {
                 temp_proc = remove_proc_node_from_front(&*process_queue);
                 add_proc_node(&*ready_queue,temp_proc);
-                printf("Adding: %d\n", temp_proc.pid);
             }
             else
             {
@@ -53,26 +53,28 @@ int RR(struct _process **process_queue, struct _process **ready_queue, struct _p
             }
         }
         
+        // re add processes
         while(temp_queue)
         {
             temp_proc = remove_proc_node_from_front(&temp_queue);
             add_proc_node(&*ready_queue,temp_proc);
         }
-            
+        
+        // fix for empty queue    
         if(!*process_queue)
             next_run_time = INT_MAX;
-                     
-        printf("Next run time: %d\n",next_run_time);             
-                     
+            
+        //fix for long wait between processes                 
         isempty = 0;
-                    
+        
+        //start at head of queue            
         iter = *ready_queue;;
         while(iter)
         {
+            // check if process is done.
             time_left = iter->burst - proc_time_done(iter);
             if(time_left > quantum)
             {
-                printf("Running process: %d\n",iter->pid);
                 //add info to time done
                 temp_time_data.start_time = runtime - first_proc_time;
                 temp_time_data.run_time = quantum;
@@ -81,12 +83,12 @@ int RR(struct _process **process_queue, struct _process **ready_queue, struct _p
                 runtime += quantum;
                 temp_ptr = iter;
                 iter = iter->next;
+                //move process to temp queue to re add later
                 temp_proc = remove_proc_node(&*ready_queue, temp_ptr);
                 add_proc_node(&temp_queue,temp_proc);
             }
             else
             {
-                printf("Running process: %d\n",iter->pid);
                 //add info to time done
                 //move runtime forward by time_left
                 temp_time_data.start_time = runtime - first_proc_time;
@@ -103,20 +105,17 @@ int RR(struct _process **process_queue, struct _process **ready_queue, struct _p
             
             if(runtime >= next_run_time)
             {
-                printf("runtime >= next_run_time\n");
+                // break to load other processes
                 break;    
             }
-            printf("Run Time: %d\n",runtime);
             
         }
         
         if(!isempty)
         {
+            //advance time in case there is long delay between processes
             runtime++;
-            printf("-------------------------\n");
-        }
-        
-        printf("Queue lengths: %d %d %d\n", queue_length(*process_queue),queue_length(*ready_queue),queue_length(*done_queue));     
+        }       
     }
     return runtime;// -first_proc_time;
 }
